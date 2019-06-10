1cac155db59f28eee54660413fb7e599fc8283b8
zven21 <andzven@gmail.com>
2019-06-10 10:20:00 +0800

    first commit

---
+++ lib/exfsm.ex
@@ -0,0 +1,18 @@
+defmodule Exfsm do
+  @moduledoc """
+  Documentation for Exfsm.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exfsm.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exfsm_test.exs
@@ -0,0 +1,8 @@
+defmodule ExfsmTest do
+  use ExUnit.Case
+  doctest Exfsm
+
+  test "greets the world" do
+    assert Exfsm.hello() == :world
+  end
+end
